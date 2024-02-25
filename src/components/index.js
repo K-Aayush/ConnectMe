59126decdc1a49552e3818389fd663cd780ac04b
Aayush <ayushkarkee6@gmail.com>
2024-02-25 19:40:39 +0545

    dashbaord header

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Button from "./Button";
 import HomePage from "./Home";
 import NavBar from "./NavBar";
 import AuthModal from "./AuthModal";
+import Header from "./Header";
 
 export {
     Button,
     HomePage,
     NavBar,
-    AuthModal
+    AuthModal,
+    Header
 }
