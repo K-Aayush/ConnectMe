b7380a7d1baf8123b89cca16e4caa74ba776f1ea
Aayush <ayushkarkee6@gmail.com>
2024-02-27 16:33:06 +0545

    message side bar

--- src/components/ChatMenu.tsx
+++ src/components/ChatMenu.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+"use client"
+
+import { useState } from 'react'
 import { LuMessagesSquare } from "react-icons/lu";
+import { RxCross1 } from "react-icons/rx";
 
 const ChatMenu = () => {
+    const [messageMenu, setMessageMenu] = useState<boolean>(false)
+
     return (
         <div>
-            <LuMessagesSquare size={30} />
+            <div onClick={() => setMessageMenu(true)} className="cursor-pointer p-3 bg-gray-100 hover:bg-gray-200 text-gray-600 rounded-full">
+                <LuMessagesSquare size={30} />
+            </div>
+
+            <div className={`fixed h-full w-screen bg-black/50 backdrop-blur-sm top-0 right-0 z-10 -translate-x-full transition-all ${messageMenu && "translate-x-0"}`}>
+                <div className="bg-white flex-col absolute left-0 top-0 h-screen p-4 gap-8 z-50 w-56">
+                    <div onClick={() => setMessageMenu(false)} className="mb-8 text-3xl cursor-pointer rounded-full hover:bg-gray-100 w-fit p-3">
+                        <RxCross1 />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,7 @@ import { ChatMenu } from '.';
 const Header = () => {
     return (
         <div className="flex items-center justify-between z-[100] mx-2 mt-2">
-            <div className="cursor-pointer p-3 bg-gray-100 hover:bg-gray-200 text-gray-600 rounded-full">
                 <ChatMenu />
-            </div>
 
             <img
                 className="object-contain h-[50px]"
